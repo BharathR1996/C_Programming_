f25e2d3cc1f76f7b3efbd7ff7a46b58203b59f6a
BharathR1996 <rbharath0@gmail.com>
2021-09-23 12:52:03 +0530

    Basic Datatypes in C

---
+++ DataTypesInC.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+// BASIC DATATYPES
+    // integer is basically a whole number
+    int age = 25;       // no quotation or paranthesis is required
+    float decimal = 5.8;    // decimal numeber can be stored using float
+
+    // single character can be stored inside a variable
+    char grade = 'A'; // A character has to be stored using Single quotes
+
+    //To Create a String
+    char phrase[] = "My Name is Bharath"; // To create a string we need to use double quotes
+
+    printf("The variable of type integer is storing %d\n", age);
+    printf("The variable of type float is storing %f\n", decimal);
+    printf("The variable of type char is storing %c\n", grade);
+    printf("The variable of type phrase is storing %s", phrase);    
+
+    return 0;
+}
