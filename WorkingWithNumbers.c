e3785102d2957d8595ebe7092ed5290e6795c2c9
BharathR1996 <rbharath0@gmail.com>
2021-09-23 14:48:15 +0530

    Update_v2

--- WorkingWithNumbers.c
+++ WorkingWithNumbers.c
@@ -2,6 +2,9 @@
 
 int main(){
 
-    printf("%f", 4.0 + 5.5);
+    printf("%f\n", 4.0 + 5.5);  // doing this operation between two float numbers will result in float
+    printf("%f\n", 5.0 * 4);  // doing this operation between one float and one integer will result in float
+    printf("%f\n", 5 + 3);  // doing this operation between two decimal numbers and since format 
+                            // specifier is float we will get output as 0.000000
     return 0;
 }
