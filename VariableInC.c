dbe02d0840d068bd0dee74e009a32dd9e9c26453
BharathR1996 <rbharath0@gmail.com>
2021-09-23 11:56:09 +0530

    This program is demonstration of using Variables

--- VariableInC.c
+++ VariableInC.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    printf("There once was a man named George\n");
-    printf("He was 70 Years old. \n");
-    printf("He really liked the name George\n");
-    printf("but did not like being 70\n");
+    printf("There once was a man named John\n");
+    printf("He was 35 Years old. \n");
+    printf("He really liked the name John\n");
+    printf("but did not like being 35\n");
     return 0;
 }
+
+// Here I have not used any variables and now if i want to change 
+// the name from George to John and age from 70 to 35 then
+// i need to edit the printf statement
