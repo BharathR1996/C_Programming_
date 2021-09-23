0e9267e98024ce9d572b98b00606d90a0124db86
BharathR1996 <rbharath0@gmail.com>
2021-09-23 11:19:13 +0530

    My first C program from Visual Studio Editor

---
+++ MyFirstProgram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>   // header file
+
+void main(){        //method is just like a container where we can put our code
+    
+    printf("Hello World!!!\n"); //printing out the text on the screen
+    printf("Hi Bharath here");
+}
+
+// This C code has to be complied which will convert the langauge above 
+// into a binary language which is understood by the computer
