a66b34ebc301f440effea751d43733a7d550ef90
BharathR1996 <rbharath0@gmail.com>
2021-09-23 11:52:57 +0530

    Printing a Triangle using printf statement

---
+++ VariableInC.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("There once was a man named George\n");
+    printf("He was 70 Years old. \n");
+    printf("He really liked the name George\n");
+    printf("but did not like being 70\n");
+    return 0;
+}
