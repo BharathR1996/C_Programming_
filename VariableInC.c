b5d9995baa8684d91a121ee06ab2dab88ee07d75
BharathR1996 <rbharath0@gmail.com>
2021-09-23 12:13:17 +0530

    Updated on using variables

--- VariableInC.c
+++ VariableInC.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("There once was a man named John\n");
-    printf("He was 35 Years old. \n");
-    printf("He really liked the name John\n");
-    printf("but did not like being 35\n");
+    // Here i will be creating two varibles one for name and one for age
+    // variables are containers and it is a good practice to provide understandable names for the variable
+   
+    char Name[] = "John";   // collection of characters
+    int age = 35;           // integer value stored in a variable
+
+    printf("There once was a man named %s\n", Name);    // When we use %s inside of the printf statement 
+                                                        // we want to insert another string
+    printf("He was %d Years old. \n", age);             // when we use %d inside of printf statement
+                                                        // we want to insert an integer
+    printf("He really liked the name %s\n", Name);
+    printf("but did not like being %d\n", Name);
     return 0;
 }
 
