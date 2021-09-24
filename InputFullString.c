10d5d336f4b3b68d97d97681eeee42db8e4b40c3
BharathR1996 <rbharath0@gmail.com>
2021-09-24 13:18:03 +0530

    Input full string from user in C_v2

--- InputFullString.c
+++ InputFullString.c
@@ -6,7 +6,7 @@
     char name1[20];
     printf("Enter the name: \n");
     fgets(name1,20, stdin);          // fgets(variable name to store data, size of the variable, stdin)
-    printf("Your Name is %s hello",name1); // One disadvantage here is that when we use fgets a new line will be printed
+    printf("Your Name is %s",name1); // One disadvantage here is that when we use fgets a new line will be printed
                                     // When we press enter key it will be considered as new line character
                                     // and will be stored inside the string. 
 
