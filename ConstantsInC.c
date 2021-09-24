a34888681cf99dc15bb3681132b77839dbf6c845
BharathR1996 <rbharath0@gmail.com>
2021-09-24 11:08:55 +0530

    Constants in C_v1

--- ConstantsInC.c
+++ ConstantsInC.c
@@ -11,9 +11,11 @@ int main(){
     /* how to use a constant variable */
     int const num1 = 5;         // constant numbers are read only variables.
                                 // we cannot write data into the variables.
+    const int num2 = 8;         // we can declare a constant integer as 
+                                                    // int const variable_name (OR) const int variable_name
 
     printf("%d\n", num1);
-
+    printf("%d\n", num2);
     //num1 = 8;               // this gives me an error i.e. assignment of read only variable
     
     
