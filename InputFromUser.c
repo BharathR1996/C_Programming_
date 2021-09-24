92fcc314f52d01051b783bbe76d7f905fb1f6d73
BharathR1996 <rbharath0@gmail.com>
2021-09-24 13:11:09 +0530

    Input from User in C_v3

--- InputFromUser.c
+++ InputFromUser.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 int main(){
-    
     // how to read a character
     char grade;
     printf("Enter your Grade: \n");
@@ -11,23 +10,15 @@ int main(){
     // how to read a string
     char name[20];
     printf("Enter your name: ");
-    scanf("%s", name);      // here it doesn't consider if we provide space in middle
-                            // Ex: John Smith is the name provided but it will consider only John
+    scanf("%s", name);              // here it doesn't consider if we provide space in middle
+                                    // Ex: John Smith is the name provided but it will consider only John
     printf("Your Name is %s\n", name);
-
-    // how to read a complete string
-    char name1[20];
-    printf("Enter the name: \n");
-    fgets(name,20, stdin);          // fgets(variable name to store data, size of the variable, stdin)
-    printf("Your Name is %s hello",name); // One disadvantage here is that when we use fgets a new line will be printed
-                                    // When we press enter key it will be considered as new line character
-                                    // and will be stored inside the string. 
     
     // how to read an integer value
-    int Age;        // declaring a variable
+    int Age;                        // declaring a variable
     printf("Enter your Age: \n");
-    scanf("%d", &Age);  // It is used to take input from the user
-                        // &Age is known as a pointer here
+    scanf("%d", &Age);              // It is used to take input from the user
+                                    // &Age is known as a pointer here
     printf("You are %d years old\n", Age);
     
     //how to read a double value
