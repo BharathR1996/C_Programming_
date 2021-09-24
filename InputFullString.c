5fdcacf394999a829bace8c1a5598836722c98e5
BharathR1996 <rbharath0@gmail.com>
2021-09-24 13:15:53 +0530

    Input Full String from User in C_v1

---
+++ InputFullString.c
@@ -0,0 +1,14 @@
+  #include<stdio.h>
+
+  int main(){
+  
+    // how to read a complete string
+    char name1[20];
+    printf("Enter the name: \n");
+    fgets(name1,20, stdin);          // fgets(variable name to store data, size of the variable, stdin)
+    printf("Your Name is %s hello",name1); // One disadvantage here is that when we use fgets a new line will be printed
+                                    // When we press enter key it will be considered as new line character
+                                    // and will be stored inside the string. 
+
+    return 0;
+  }
