f43ce70bcab54b534764c58b4746868efa2a7b1e
BharathR1996 <rbharath0@gmail.com>
2021-09-24 17:01:21 +0530

    Basic Reverse of Character using printf Statement in C

---
+++ BasicReverseCharacter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    char data1 = 'X';
+    char data2 = 'M';
+    char data3 = 'L';
+
+    printf("The reverse of %c%c%c is %c%c%c\n", data1, data2, data3, data3, data2,data1);
+    return 0;
+      
+}
