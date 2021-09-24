f03d3bc866728c9ab2194e5f36ad0ed619fbfa13
BharathR1996 <rbharath0@gmail.com>
2021-09-24 14:10:28 +0530

    C Program to print your Name, date of birth and mobile number

---
+++ .vscode/NameDOBContactinC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Name   : Alexandra Abramov\n");
+    printf("DOB    : January 19, 1996\n");
+    printf("Mobile : 99-9999999999\n");
+
+    return 0;
+}
